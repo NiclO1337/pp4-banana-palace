211794be1cbff9fff455ab174e2ee23940283215
NiclO1337 <niclas.hugdahl@gmail.com>
2024-02-20 00:59:26 +0100

    Add logo as navbar-brand and style responsively
    Change JS to use jQuery instead of vanilla JS

--- static/js/script.js
+++ static/js/script.js
@@ -2,24 +2,22 @@ console.log("Hello World!")
 
 
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// When the user scrolls down 80px from the top of the document,
+// resize the navbar's padding and the logo's font size
 window.onscroll = function () {
     scrollFunction();
-  };
+};
 
-  function scrollFunction() {
+function scrollFunction() {
     if (
       document.body.scrollTop > 80 ||
       document.documentElement.scrollTop > 80
     ) {
-      document.getElementsByClassName("container-fluid")[0].style.padding =
-        "30px 10px";
-      document.getElementsByClassName("navbar-brand")[0].style.fontSize =
-        "25px";
+
+        $("div.container-fluid")[0].style.padding =
+            "0";
     } else {
-      document.getElementsByClassName("container-fluid")[0].style.padding =
-        "80px 10px";
-      document.getElementsByClassName("navbar-brand")[0].style.fontSize =
-        "35px";
+        $("div.container-fluid")[0].style.padding =
+            "80px 0";
     }
-  }
+}
