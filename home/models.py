2fbbd64d8548c8c6571d0c120899b3fede198f08
NiclO1337 <niclas.hugdahl@gmail.com>
2024-02-19 01:31:21 +0100

    Add database and create superuser
    Add query to the database to see if user was created
    
    Install psycopg2, dj-database-url and gunicorn
    
    Prepare to deploy to heroku

--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+
+
+
+
+
+
+# Query the database
+
+
+# print('\n'
+#     f'{'Name:':<14}',
+#     f'{'Email address:':<25}',
+#     'Is superuser:',
+#     sep=" | "
+# )
+
+# print(
+#     '----------------------------------------------------------'
+# )
+# for user in User.objects.raw("SELECT * FROM auth_user"):
+#     print(
+#         f'{user.username:<14}',
+#         f'{user.email:<25}',
+#         user.is_superuser,
+#         sep=" | "
+#     )
