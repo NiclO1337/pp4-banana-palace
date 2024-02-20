8cdad2647fa666a291e59299252ae6f8ba72498b
NiclO1337 <niclas.hugdahl@gmail.com>
2024-02-20 02:29:24 +0100

    Add brand logo under nav and style navbar
    Adjust JS to display one of the logos depending on scroll
    
    Style navbar based on scroll and screen size
    
    Add main with class of container to base.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 console.log("Hello World!")
 
 
-
 // When the user scrolls down 80px from the top of the document,
 // resize the navbar's padding and the logo's font size
 window.onscroll = function () {
@@ -10,14 +9,27 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (
-      document.body.scrollTop > 80 ||
-      document.documentElement.scrollTop > 80
+      document.body.scrollTop > 10 ||
+      document.documentElement.scrollTop > 10
     ) {
+        $('#header-container')[0].style.padding = "0";
 
-        $("div.container-fluid")[0].style.padding =
-            "0";
+        $('main.container')[0].style.marginTop = "15rem";
+
+        $('.navbar-brand')[0].style.display = "block";
+        $('#main-logo')[0].style.display = "none";
+
+        $('#navbarNav')[0].classList.remove('justify-content-center');
+        $('#navbarNav')[0].classList.add('justify-content-end');
     } else {
-        $("div.container-fluid")[0].style.padding =
-            "80px 0";
+        $('#header-container')[0].style.padding = "0 0 5rem";
+
+        $('main.container')[0].style.marginTop = "6rem";
+
+        $('.navbar-brand')[0].style.display = "none";
+        $('#main-logo')[0].style.display = "block";
+
+        $('#navbarNav')[0].classList.remove('justify-content-end');
+        $('#navbarNav')[0].classList.add('justify-content-center');
     }
 }
