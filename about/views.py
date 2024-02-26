af3bced67ca82cf4014b35001bece02f70f164f2
NiclO1337 <niclas.hugdahl@gmail.com>
2024-02-26 02:31:52 +0100

    Add about app and get it to display a text

---
+++ about/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class About(TemplateView):
+
+    template_name = 'about/about.html'
+
