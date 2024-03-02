4f1ba0e84bf8447652c1d4de7426fcdaf39d821f
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-03 00:51:37 +0100

    Refactor to automatically style all account template pages
    Change regular base and custom pages back to use block page-content
    
    Style account base to add page-content and get content from other files
    
    Adjust css target on buttons to style all sub-pages
    
    Import messages to display all standard messages

--- banana_palace/settings.py
+++ banana_palace/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,6 +164,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+MESSAGE_TAGS = {
+    messages.INFO: '',
+    50: 'critical',
+}
 
 
 # Email backend
