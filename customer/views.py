1ebf18d09d91ba64212297bf23774f3a1d53c6d4
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-06 17:19:56 +0100

    Add account page and define an account view
    Update template-template.html with extra added blocks
    
    Add template-template.html code in account.html

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Account(request):
+    """
+    Display users account page
+    """
+    return render(request, 'account/account.html')
