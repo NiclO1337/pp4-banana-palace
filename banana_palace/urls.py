98896670cdb8d0985cc9df1246b6461e557362de
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-06 16:50:30 +0100

    Add urlpattern for new account app

--- banana_palace/urls.py
+++ banana_palace/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('about/', include('about.urls'), name='about-urls'),
+    path('account/', include('customer.urls'), name='customer-urls'),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls, name='admin:index'),
     path('', include('home.urls'), name='home-urls'),
