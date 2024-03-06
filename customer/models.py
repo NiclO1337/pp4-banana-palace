77b53fc253c00094e6fc79db63e640ba65a4c97e
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-06 03:45:59 +0100

    Add Customer model to database
    Write class in home/models.py and add to admin.py
    
    Makemigrations and migrate to database

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    """
+    Creates a customer related to :model:`auth.User`
+    """
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="customer"
+    )
+    has_discount = models.BooleanField(default=False)
+    has_clicked = models.BooleanField(default=False)
+    is_owner = models.BooleanField(default=False)
