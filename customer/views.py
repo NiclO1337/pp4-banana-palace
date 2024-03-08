8459fead1d4fc4d979bf555976b3313b3f009f17
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-09 00:20:03 +0100

    Add defensive programming so only logged in user can access account page

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required()
 def account(request):
     """
     Display users account page
