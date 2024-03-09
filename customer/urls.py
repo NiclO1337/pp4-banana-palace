dad578a82ac7299f0b13ed31d3ddc240f22d92cd
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-09 01:20:53 +0100

    Add edit_account page and style as accountmanagement
    Used Codemy guide, link is in credit section of README

--- customer/urls.py
+++ customer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.account, name='account'),
+    path('edit_account/', views.AccountEditView.as_view(), name='edit_account'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 # Create your views here.
 @login_required()
@@ -8,3 +11,11 @@ def account(request):
     Display users account page
     """
     return render(request, 'account/account.html')
+
+class AccountEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'account/edit_account.html'
+    success_url = reverse_lazy('account')
+
+    def get_object(self):
+        return self.request.user
