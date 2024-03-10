a310683f14de7d64904f01df92abf925475ffb01
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-10 21:44:28 +0100

    Add javascript to show and animate click me text on account page

---
+++ static/js/account-page.js
@@ -0,0 +1,12 @@
+// Makes it more clear that shaking symbol is supposed to be clicked
+// Adds opacity to make "Click me!" text visible and change animation
+$(document).ready(function() {
+    setInterval(function() {
+
+        $('#click-me')[0].style.opacity = '1';
+        $('#click-me').parent('a').parent('p').addClass("fa-beat-fade");
+        $('#click-me').siblings('i').removeClass("fa-shake");
+
+    }, 10000)
+})
+
