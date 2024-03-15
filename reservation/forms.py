5b5d66ded17c9d6be00448e8bd614e760390d009
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-15 21:24:49 +0100

    Add better looking date input

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
+from django.contrib.admin.widgets import AdminDateWidget
 from django import forms
 from .models import Table, Reservation
+from datetime import datetime, date
 
 
 class PickDateForm(forms.ModelForm):
-    date = forms.DateField(widget=forms.SelectDateWidget())
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
 
     class Meta:
         model = Table
