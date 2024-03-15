6494110ba34c7a5523b56f6286841d3d42b33fa4
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-15 15:34:39 +0100

    Refactor, add code comments and remove unused code

--- reservation/views.py
+++ reservation/views.py
@@ -11,43 +11,25 @@ from datetime import date
 @login_required
 def reservation_page(request):
 
+
+    # Get number of avalible tables from the resturant
+    # associated with the logged in user
     nr_of_tables = request.user.customer.restaurant.avalible_tables
 
-    today = date.today()
-    string_today = str(today)
+    # get all tables for todays date (today is when page is loaded)
+    tables = Table.objects.filter(date=date.today())
 
-    # tables = Table.objects.filter()
-
-    tables = Table.objects.filter(date=today)
-
-    # for table in tables:
-    #     if table.date not in today:
-    #         for table in nr_of_tables:
-    #             Table()
-    #             table.save
-
+    # if there are no tables, create tables and save them to the database
+    # nr of tables created depends on restaurants nr of avalible tables
     if not tables:
-        tables = []
         for table in range(nr_of_tables):
             new_table = Table()
             new_table.save()
             tables.append(new_table)
-        return tables
-
-
-    # tables = [
-    #     Table(),
-    # # ]
-    # Table.objects.bulk_create(tables)
 
 
     return render(request, 'reservation/reservation.html',
-                  {'range': range(nr_of_tables),
-                   'tables': tables,
-                   'today': today,
-                   'string_today': string_today,
-                   'nr_of_tables': nr_of_tables,
-                   })
+                  {'tables': tables})
 
 
 
@@ -64,21 +46,6 @@ def reservation_page(request):
 
 
 
-# Create number of avalible tables from Restaurant model
-# Get restaurant by name, get restarant.avalible_tables value
-# Tables need specific position.
-
-# or create finished tables with position in html with unique ID's,
-# and create database tables from the html when choosing it
-
-# Or create by model as planned and use JS to position them correctly,
-# relative parent, absolute children.
-
-
-
-"""
-Continue as planned and learn from it
-"""
 
 
 
