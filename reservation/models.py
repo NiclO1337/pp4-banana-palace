536062a29729a3bffd5ee8ed9cf5f6075544d45c
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-18 00:45:14 +0100

    Add ordering to models for admin page

--- reservation/models.py
+++ reservation/models.py
@@ -9,6 +9,9 @@ class Table(models.Model):
     date = models.DateField(default=date.today)
     reserved = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ['date']
+
     def __str__(self):
         return f'Table ID: {self.id} - {self.date}'
 
@@ -30,6 +33,8 @@ class Reservation(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE,
                                    default=2)
 
+    class Meta:
+        ordering = ['table__date']
 
     # Do not think I need this one since user is a FK and I should be able
     # to get it by targetting User.customer.has_discount
