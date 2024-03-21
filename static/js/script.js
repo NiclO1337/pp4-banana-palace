ab2cf8af68d15c3d9e3ffbeffa98789a99639210
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-21 21:46:45 +0100

    Update footer links with urls and disable js script

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
 // Add on click function to footer links extra to display an error message
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    let errorMessage = $('.error-msg-links')[0];
+//     let errorMessage = $('.error-msg-links')[0];
 
-    $('.footer-links-extra').children('li').click(function () {
+//     $('.footer-links-extra').children('li').click(function () {
 
-        errorMessage.style.display = "block";
-        $('.footer-links-extra').children('li').addClass("d-none");
+//         errorMessage.style.display = "block";
+//         $('.footer-links-extra').children('li').addClass("d-none");
 
-        window.setTimeout(() => {
-            errorMessage.style.opacity = "1";
-        }, 100)
-        window.setTimeout(() => {
-            errorMessage.style.opacity = "0";
-        }, 5100)
-        window.setTimeout(() => {
-            errorMessage.style.display = "none";
-            $('.footer-links-extra').children('li').removeClass("d-none");
-        }, 6100)
-    })
-})
+//         window.setTimeout(() => {
+//             errorMessage.style.opacity = "1";
+//         }, 100)
+//         window.setTimeout(() => {
+//             errorMessage.style.opacity = "0";
+//         }, 5100)
+//         window.setTimeout(() => {
+//             errorMessage.style.display = "none";
+//             $('.footer-links-extra').children('li').removeClass("d-none");
+//         }, 6100)
+//     })
+// })
