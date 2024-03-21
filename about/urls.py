e1940ada47b5958337eeb9698dd0d6e962fac99b
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-21 21:36:30 +0100

    Add terms and condition link to reserve table page
    Add button on terms and condition page to go back to booking page

--- about/urls.py
+++ about/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.About.as_view(), name='about'),
     path('story/', views.OurStory.as_view(), name='our-story'),
     path('terms-and-conditions/', views.TermsAndConditions.as_view(),
-         name='terms'),
+         name='terms-and-conditions'),
     path('privacy-policy/', views.PrivacyPolicy.as_view(),
          name='privacy-policy'),
     path('cookie-policy/', views.CookiePolicy.as_view(),
