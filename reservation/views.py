c80ac8cd93278bbed2c89c81b29a10811d0fd699
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-24 12:40:26 +0100

    Fix part of non-working reservation edit

--- reservation/views.py
+++ reservation/views.py
@@ -144,15 +144,51 @@ necessairy information below')
 
 def edit_reservation(request, reservation_id):
 
+    nr_of_tables = request.user.customer.restaurant.avalible_tables
     reservation = get_object_or_404(Reservation, pk=reservation_id)
 
     tables = Table.objects.filter(date=reservation.table.date).order_by('id')
     reservations = Reservation.objects.filter(
         table__date=reservation.table.date)
 
+    if request.method == "POST":
+        date_form = PickDateForm(data=request.POST)
+        if date_form.is_valid():
+            selected_date = date_form.cleaned_data['date']
+            tables = Table.objects.filter(date=selected_date).order_by('id')
+            reservations = Reservation.objects.filter(
+                table__date=selected_date)
+            if not tables:
+                tables = []
+                for table in range(nr_of_tables):
+                    new_table = Table(date=selected_date)
+                    new_table.save()
+                    tables.append(new_table)
+
+                reservations = []
+
+                random_number = random.randint(10,20)
+                list_of_nums = list(range(1, nr_of_tables))
+                random.shuffle(list_of_nums)
+                list_of_random_nums = []
+                for num in range(random_number):
+                    random_num = list_of_nums[num]
+                    list_of_random_nums.append(random_num)
+
+                for table in list_of_random_nums:
+                    new_reservation = Reservation(table=tables[table])
+                    new_reservation.save()
+                    reservations.append(new_reservation)
 
 
-    date_form = PickDateForm()
+            return render(request, 'reservation/reservation.html',
+                          {'tables': tables,
+                           'date_form': date_form,
+                           'reservations': reservations,
+                           })
+
+    date_form = PickDateForm(initial={
+            'date': reservation.table.date,})
 
     return render(request, 'reservation/reservation.html',
                   {'tables': tables,
