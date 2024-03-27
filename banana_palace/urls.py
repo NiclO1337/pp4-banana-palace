66557c51caa49318d40c217ead5c634104bdc5cf
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-27 16:02:53 +0100

    Fix mistake in project urls file

--- banana_palace/urls.py
+++ banana_palace/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('account/', include('customer.urls'), name='customer-urls'),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls, name='admin:index'),
-    path('menu/', include('menu.urls', name='menu-urls')),
+    path('menu/', include('menu.urls'), name='menu-urls'),
     path('reservation/', include('reservation.urls'), name='reservation-urls'),
     path('', include('home.urls'), name='home-urls'),
 ]
