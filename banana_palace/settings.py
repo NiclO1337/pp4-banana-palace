5d35c3144246fe5dabdf879871fcb71add12f6ba
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-27 22:15:00 +0100

    Finish menu model and migrate #29
    Set up file storage in settings and install Pillow

--- banana_palace/settings.py
+++ banana_palace/settings.py
@@ -200,6 +200,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Whitenoise Storage Class
 # Apply compression but don’t want the caching behaviour
--- menu/models.py
+++ menu/models.py
@@ -12,11 +12,15 @@ class Menu(models.Model):
         ('Kids', 'Kids'),
     ]
 
-    title = models.CharField()
-    content = models.TextField()
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=150)
     price = MoneyField(max_digits=3, decimal_places=0, default_currency='EUR')
     category = models.CharField(choices=CATEGORY_CHOICES)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='menu/')
     is_current = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.title} ----- {self.category} ----- {self.is_current}'
