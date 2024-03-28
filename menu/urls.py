2541211694b8ae0c7b7aedd7cc8f231c7a0182ba
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-28 23:57:48 +0100

    Add delete button visible to the owner to each item

--- menu/urls.py
+++ menu/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.menu_page, name='menu_page'),
-    path('edit/<int:menu_item_id>', views.edit_menu_item, name='edit_menu_item'),
+    path('edit/<int:menu_item_id>',
+         views.edit_menu_item, name='edit_menu_item'),
     path('add/', views.add_menu_item, name='add_menu_item'),
-    path('delete/', views.delete_menu_item, name='delete_menu_item'),
+    path('delete/<int:menu_item_id>',
+         views.delete_menu_item, name='delete_menu_item'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
