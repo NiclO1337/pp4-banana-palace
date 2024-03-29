b21ea3205ec1b8d35b58d512dfef5f049c63cc69
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-29 01:58:15 +0100

    Change index page to function based view and use menu item variable

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import TemplateView
+from menu.models import MenuItem
+from django.shortcuts import render
 
-# Create your views here.
-class Home(TemplateView):
 
-    template_name = 'home/index.html'
+def home_page(request):
 
+    menu_items = MenuItem.objects.filter(is_current=True)
+
+    return render(request, 'home/index.html', {'menu_items': menu_items,})
