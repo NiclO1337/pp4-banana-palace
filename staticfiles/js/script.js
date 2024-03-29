bcbd83338c4e3dabea2ca279832409a3869c7200
NiclO1337 <niclas.hugdahl@gmail.com>
2024-03-30 00:02:24 +0100

    Collectstatic for deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,25 +1,9 @@
-// Add on click function to footer social media icons
-// to display an error message about missing links
-$(document).ready(function () {
+$(document).ready(function() {
 
-    let errorMessage = $('.error-msg-links')[0];
-    let socialMediaIcons = $('.social-media-symbols li');
+    $('#navbarNav')[0].classList.remove('mt-3');
 
-    socialMediaIcons.on('click', socialMediaIcons, function () {
-        errorMessage.style.display = "block";
-        socialMediaIcons.css('pointer-events', 'none');
-        socialMediaIcons.css('color', 'var(--color-tertiary-light)');
-
-        window.setTimeout(() => {
-            errorMessage.style.opacity = "1";
-        }, 100)
-        window.setTimeout(() => {
-            errorMessage.style.opacity = "0";
-        }, 5100)
-        window.setTimeout(() => {
-            errorMessage.style.display = "none";
-            socialMediaIcons.css('pointer-events', 'all');
-            socialMediaIcons.css('color', 'var(--color-primary-bg');
-        }, 6100)
-    })
+    $('#header-address-nav')[0].classList.remove('d-sm-block')
+    $('#header-phone-nav')[0].classList.remove('d-sm-block')
+    $('#header-address')[0].classList.remove('d-block')
+    $('#header-phone')[0].classList.remove('d-block')
 })
